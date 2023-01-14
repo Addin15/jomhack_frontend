8a1b7080f45c07d1e2c20727ba2a6349ea03486e
Addin15 <addin.utb15@gmail.com>
2023-01-14 21:38:04 +0800

    update profile

---
+++ lib/pages/assestment.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AssestmentPage extends StatefulWidget {
+  const AssestmentPage({super.key});
+
+  @override
+  State<AssestmentPage> createState() => _AssestmentPageState();
+}
+
+class _AssestmentPageState extends State<AssestmentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
